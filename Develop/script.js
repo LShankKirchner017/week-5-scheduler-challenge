f24b5a4db0f8639f8407aef166b20a26b275aea1
Lindsay <lshank017@gmail.com>
2023-07-07 12:41:53 -0500

    worked more on JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,18 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the HTML
 $(document).ready(function () {
-   
-      
+         
     $(function(){
+      // how we get the current time
        var now = dayjs();
+
+      // how we compare the now hour to the schedule hour 
       var $timeBlocks = $('.time-block')
       $timeBlocks.each(function(){
         var $timeBlocks = $(this)
         var nowHour = now.hour()
         var blockHour = $timeBlocks.attr('id').split('-')[1]
-       
+
+       // how we assign a class to the schedule block
         var timeClass
         if (blockHour == nowHour){
           timeClass='present'
@@ -21,8 +24,8 @@ $(document).ready(function () {
         $timeBlocks.addClass(timeClass)
       })
     })
-   
+   // how we display the current date
   dayjs().format("L LT");
   var now = dayjs().format("dddd, MMMM D, YYYY");
   $("#currentDay").text(now).appendTo("header");
-});
+})
